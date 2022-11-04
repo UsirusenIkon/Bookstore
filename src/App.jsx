eb5f4a64e3ca943efcf488aac4ad5fa7e353f666
Usirusen Ikon <lexicon048@gmail.com>
2022-11-04 14:49:03 +0100

    Install and edit react files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+import './App.css';
+import Books from './components/Books/Books';
+import Categories from './components/Categories/Categories';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h1>Bookstore CMS</h1>
+        <ul>
+          <li><Link to="/">Books</Link></li>
+          <li><Link to="/Categories">Categories</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
