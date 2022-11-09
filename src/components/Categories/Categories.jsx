a1c1ff3b736ac838b27c4b2ae9c0e274f1c31c9d
Usirusen Ikon <lexicon048@gmail.com>
2022-11-09 15:23:30 +0100

    Add linters

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,10 +1,22 @@
+import { useDispatch } from "react-redux";
+import { checkStatus } from "../Redux/Categories/Categories";
 import Buttons from '../Buttons/Buttons';
 import './Categories.css'
 
 const Categories = () => {
+  const dispatch = useDispatch()
+
+  const checkStatusHandler = () => {
+    console.log('goat');
+    dispatch (
+      checkStatus()
+    )
+  }
+
   return (
     <div>
       <Buttons 
+        onClick={checkStatusHandler}
         className="btn" 
         name="Check status" 
       />
